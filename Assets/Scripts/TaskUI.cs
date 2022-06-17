f0a8cd1856280a7357dea1160a63a46a9773f885
AljonRT <68255833+AljonRT@users.noreply.github.com>
2022-06-18 02:06:25 +0800

    Initial implementation of task list uii

---
+++ Assets/Scripts/TaskUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TaskUI : MonoBehaviour
+{
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI description;
+}
---
+++ Assets/Scripts/TaskUpdate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskUpdate : MonoBehaviour
+{
+    [SerializeField]
+    private TaskUI taskPrefab;
+    [SerializeField]
+    private ScriptableGameOBJ playerStats;
+    [SerializeField]
+    private GameObject content;
+
+
+    private void Start()
+    {
+        for(int i = 0; i < playerStats.filteredList.tasksList.Count; i++)
+        {
+            TaskUI temp = GameObject.Instantiate(taskPrefab, content.transform);
+            temp.title.text = playerStats.filteredList.tasksList[i].title;
+            temp.description.text = playerStats.filteredList.tasksList[i].notes;
+        }
+    }
+
+}
